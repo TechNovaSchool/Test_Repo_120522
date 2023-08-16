757bbbb7940f11ec809e0bf923d48783af988d39
Gkenzhebekova96 <Brazil52@>
2023-08-16 22:17:45 +0400

    We have added some changes

---
+++ Test_demo_2023.java
@@ -0,0 +1,5 @@
+public class Test_demo_2023 {
+    public static void main(String[] args) {
+        System.out.println("Test Demo Hello Hello My name is Guljamal");
+    }
+}
--- test_demo_aida.java
+++
@@ -1,5 +0,0 @@
-public class test_demo_aida {
-    public static void main(String[] args) {
-        System.out.println("Hello Good People");
-    }
-}
