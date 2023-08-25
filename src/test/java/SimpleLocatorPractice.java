65b423212f6b97d8e0dad256472c07c75f657221
Gkenzhebekova96 <Brazil52@>
2023-08-25 18:20:44 +0400

    Homework

--- src/test/java/SimpleLocatorPractice.java
+++ src/test/java/SimpleLocatorPractice.java
@@ -2,33 +2,32 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
+
+
 
 public class SimpleLocatorPractice {
     public static void main(String[] args) {
-
-
         WebDriverManager.chromedriver().setup();
-        WebDriver driver=new ChromeDriver();
+        WebDriver driver =new ChromeDriver();
 
 
-      //ChromeOptions options = new ChromeOptions();
-      // options.addArguments("--remote-allow-origins=*");
-        // driver.get("https://www.google.com");
+           // ChromeOptions options = new ChromeOptions();
+            // options.addArguments("--remote-allow-origins=*");
+            // driver.get("https://www.google.com");
 
 
-      // driver.findElement(By.linkText("Today's Deals")).click();
-      // driver.navigate().back();
+            // driver.findElement(By.linkText("Today's Deals")).click();
+            // driver.navigate().back();
 
-       //driver.findElement(By.name("field-keywords")).sendKeys("face toner");
+            //driver.findElement(By.name("field-keywords")).sendKeys("face toner");
 
-        //driver.findElement(By.className("pHiOh")).click();
+            //driver.findElement(By.className("pHiOh")).click();
 
         driver.get("https://www.etsy.com/");
-        driver.findElement(By.id("global-enhancements-search-query")).sendKeys("gifts", Keys.ENTER);
+        driver.findElement(By.id("global-enhancements-search-query")).sendKeys("gifts",Keys.ENTER);
         driver.findElement(By.tagName("h3"));
 
-    }
-}
+        }}
+
---
+++ src/test/java/Task15.java
@@ -0,0 +1,28 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+public class Task15 {
+    public static void main(String[] args) {
+        WebDriverManager.firefoxdriver().setup();
+        WebDriver driver= new FirefoxDriver();
+        driver.get("https://demoqa.com/radio-button");
+
+        driver.findElement(By.xpath("//label[@for=\"impressiveRadio\"]")).click();
+
+        System.out.println(driver.findElement(By.xpath("//p[@class=\"mt-3\"]/span")).isDisplayed());
+
+        WebElement noRadio=driver.findElement(By.xpath("//label[@for=\"noRadio\"]"));
+        if (noRadio.isSelected()){
+            System.out.println("tess passed");
+        }else {
+            System.out.println("test fail");
+        }
+
+
+
+    }
+}
